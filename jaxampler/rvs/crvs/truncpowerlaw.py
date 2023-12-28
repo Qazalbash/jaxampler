26b66841624d555dfd821ae63198a686b79c65ad
Meesum Qazalbash <meesumqazalbash@gmail.com>
2023-12-28 20:07:40 +0500

    Revert "v0.0.4 ready"
    This reverts commit 9ef08bf770e1677c1563db3ed24e1ed27aad4c20.

--- jaxampler/rvs/crvs/truncpowerlaw.py
+++ jaxampler/rvs/crvs/truncpowerlaw.py
@@ -15,7 +15,7 @@
 from functools import partial
 
 import jax
-from jax import Array, jit
+from jax import Array, jit, lax
 from jax import numpy as jnp
 from jax.typing import ArrayLike
 
@@ -38,12 +38,13 @@ class TruncPowerLaw(ContinuousRV):
 
     @partial(jit, static_argnums=(0,))
     def logZ(self) -> ArrayLike:
-        logZ_val = jnp.where(
-            self._beta == 0.0,
-            jnp.log(jnp.log(self._high) - jnp.log(self._low)),
-            jnp.log(jnp.abs(jnp.power(self._high, self._beta) - jnp.power(self._low, self._beta))) -
-            jnp.log(jnp.abs(self._beta)),
-        )
+        logZ_val = lax.cond(
+            self._alpha == -1.0, lambda _: jnp.log(jnp.log(self._high) - jnp.log(self._low)), lambda beta: lax.cond(
+                beta > 0.0,
+                lambda b: jnp.log(jnp.power(self._high, b) - jnp.power(self._low, b)) - jnp.log(b),
+                lambda b: jnp.log(jnp.power(self._low, b) - jnp.power(self._high, b)) - jnp.log(-b),
+                self._beta,
+            ), self._beta)
         return logZ_val
 
     @partial(jit, static_argnums=(0,))
@@ -54,12 +55,13 @@ class TruncPowerLaw(ContinuousRV):
 
     @partial(jit, static_argnums=(0,))
     def logcdf(self, x: ArrayLike) -> ArrayLike:
-        logcdf_val = jnp.where(
-            self._beta == 0.0,
-            jnp.log(jnp.log(x) - jnp.log(self._low)),
-            jnp.log(jnp.abs(jnp.power(x, self._beta) - jnp.power(self._low, self._beta))) -
-            jnp.log(jnp.abs(self._beta)),
-        )
+        logcdf_val = lax.cond(
+            self._alpha == -1.0, lambda _: jnp.log(jnp.log(x) - jnp.log(self._low)), lambda beta: lax.cond(
+                beta > 0.0,
+                lambda b: jnp.log(jnp.power(x, b) - jnp.power(self._low, b)) - jnp.log(b),
+                lambda b: jnp.log(jnp.power(self._low, b) - jnp.power(x, b)) - jnp.log(-b),
+                self._beta,
+            ), self._beta)
         logcdf_val -= self._logZ
         logcdf_val = jnp.where(x >= self._low, logcdf_val, -jnp.inf)
         logcdf_val = jnp.where(x <= self._high, logcdf_val, jnp.log(1.0))
@@ -67,11 +69,12 @@ class TruncPowerLaw(ContinuousRV):
 
     @partial(jit, static_argnums=(0,))
     def logppf(self, x: ArrayLike) -> ArrayLike:
-        logppf_val = jnp.where(
-            self._beta == 0.0,
-            x * jnp.log(self._high) + (1.0 - x) * jnp.log(self._low),
-            jnp.power(self._beta, -1) * jnp.log(x * jnp.power(self._high, self._beta) +
-                                                (1.0 - x) * jnp.power(self._low, self._beta)))
+        logppf_val = lax.cond(
+            self._alpha == -1.0,
+            lambda _: x * jnp.log(self._high) + (1.0 - x) * jnp.log(self._low),
+            lambda b: (1.0 / b) * jnp.log(x * b * jnp.exp(self._logZ) + jnp.power(self._low, b)),
+            self._beta,
+        )
         logppf_val = jnp.where(x >= 0.0, logppf_val, -jnp.inf)
         logppf_val = jnp.where(x <= 1.0, logppf_val, jnp.log(1.0))
         return logppf_val
