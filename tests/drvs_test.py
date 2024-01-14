07872c0066cb107f595589b93b701babcbfb595f
mu06905 <mu06905@st.habib.edu.pk>
2024-01-14 22:35:38 +0500

    bin test fixed acc revies

--- tests/drvs_test.py
+++ tests/drvs_test.py
@@ -16,26 +16,26 @@ import sys
 
 sys.path.append("../jaxampler")
 import sys
-import pytest
+
 import jax
 import jax.numpy as jnp
-from jaxampler._src.rvs.binomial import Binomial
 
-eps = 1e-3
+from jaxampler._src.rvs.binomial import Binomial
 
 
 class TestBinomial:
 
     def test_logpmf_x(self):
-        assert jnp.allclose(Binomial(p=0.5, n=10).logpmf_x(5), jax.scipy.stats.binom.logpmf(5, 10, 0.5))
+        assert jnp.allclose(
+            Binomial(p=0.5, n=10, name="test_logpmf_p0.5").logpmf_x(5), jax.scipy.stats.binom.logpmf(5, 10, 0.5))
 
-        assert Binomial(p=0.5, n=[10, 20]).logpmf_x(5).shape == (2,)
-        assert Binomial(p=[0.5, 0.1], n=[10, 20]).logpmf_x(5).shape == (2,)
-        assert Binomial(p=[0.5, 0.1, 0.3], n=[10, 20, 30]).logpmf_x(5).shape == (3,)
+        assert Binomial(p=0.5, n=[10, 20], name="test_logpmf_n2").logpmf_x(5).shape == (2,)
+        assert Binomial(p=[0.5, 0.1], n=[10, 20], name="test_logpmf_p2n2").logpmf_x(5).shape == (2,)
+        assert Binomial(p=[0.5, 0.1, 0.3], n=[10, 20, 30], name="test_logpmf_p3n3").logpmf_x(5).shape == (3,)
 
         # when probability is very small
         assert jnp.allclose(
-            Binomial(p=0.0001, n=10).logpmf_x(5),
+            Binomial(p=0.0001, n=10, name="test_logpmf_p0.0001").logpmf_x(5),
             jax.scipy.stats.binom.logpmf(5, 10, 0.0001),
         )
 
@@ -43,41 +43,41 @@ class TestBinomial:
         assert jnp.allclose(Binomial(p=0.1, n=100000).logpmf_x(50), jax.scipy.stats.binom.logpmf(50, 100000, 0.1))
 
     def test_pmf_x(self):
-        assert jnp.allclose(Binomial(p=0.5, n=10).pmf_x(5), jax.scipy.stats.binom.pmf(5, 10, 0.5))
+        assert jnp.allclose(Binomial(p=0.5, n=10, name="test_pmf_p0.5").pmf_x(5), jax.scipy.stats.binom.pmf(5, 10, 0.5))
 
-        assert Binomial(p=0.5, n=[10, 20]).pmf_x(5).shape == (2,)
-        assert Binomial(p=[0.5, 0.1], n=[10, 20]).pmf_x(5).shape == (2,)
-        assert Binomial(p=[0.5, 0.1, 0.3], n=[10, 20, 30]).pmf_x(5).shape == (3,)
-        assert Binomial(p=[[0.5, 0.1], [0.4, 0.1]], n=[10, 20]).pmf_x(5).shape == (2, 2)
+        assert Binomial(p=0.5, n=[10, 20], name="test_pmf_n2").pmf_x(5).shape == (2,)
+        assert Binomial(p=[0.5, 0.1], n=[10, 20], name="test_pmf_p2n2").pmf_x(5).shape == (2,)
+        assert Binomial(p=[0.5, 0.1, 0.3], n=[10, 20, 30], name="test_pmf_p3n3").pmf_x(5).shape == (3,)
+        assert Binomial(p=[[0.5, 0.1], [0.4, 0.1]], n=[10, 20], name="test_pmf_p2x3n2").pmf_x(5).shape == (2, 2)
 
         # when probability is very small
         assert jnp.allclose(
-            Binomial(p=0.0001, n=10).pmf_x(5),
+            Binomial(p=0.0001, n=10, name="test_pmf_p0.0001").pmf_x(5),
             jax.scipy.stats.binom.pmf(5, 10, 0.0001),
         )
 
         # when n is very large
         assert jnp.allclose(
-            Binomial(p=0.1, n=100000).pmf_x(50),
+            Binomial(p=0.1, n=100000, name="test_pmf_p0.1n100000").pmf_x(50),
             jax.scipy.stats.binom.pmf(50, 100000, 0.1),
         )
 
     def test_cdf_x(self):
-        binomial = Binomial(p=0.2, n=12)
-        assert binomial.cdf_x(x=13) == 1
-        assert binomial.cdf_x(-1) == 0
-        assert binomial.cdf_x(9) >= 0
-        assert binomial.cdf_x <= 1
+        bin_cdf = Binomial(p=0.2, n=12, name="test_cdf")
+        assert bin_cdf.cdf_x(x=13) == 1
+        assert bin_cdf.cdf_x(-1) == 0
+        assert bin_cdf.cdf_x(9) >= 0
+        assert bin_cdf.cdf_x(9) <= 1
 
     def test_rvs(self):
-        binomial = Binomial(p=0.6, n=5)
+        bin_rvs = Binomial(p=0.6, n=5, name="tets_rvs")
         shape = (3, 4)
 
         # with key
         key = jax.random.PRNGKey(123)
-        result = binomial.rvs(shape, key)
-        assert result.shape, shape + binomial._shape
+        result = bin_rvs.rvs(shape, key)
+        assert result.shape, shape + bin_rvs._shape
 
         # without key
-        result = binomial.rvs(shape)
-        assert result.shape, shape + binomial._shape
+        result = bin_rvs.rvs(shape)
+        assert result.shape, shape + bin_rvs._shape
