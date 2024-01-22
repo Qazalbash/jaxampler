f8d442ebac9212b8f60fdc71e45b61718610308f
Meesum Qazalbash <meesumqazalbash@gmail.com>
2024-01-22 09:41:40 +0500

    resolves PR conflict :happy:

--- tests/exponential_test.py
+++ tests/exponential_test.py
@@ -23,64 +23,61 @@ sys.path.append("../jaxampler")
 from jaxampler.rvs import Exponential
 
 
-class TestBinomial:
+class TestExponential:
     def test_invalid_params(self):
         with pytest.raises(AssertionError):
-            Exponential(-1.0)
+            Exponential(scale=-1.0)
         with pytest.raises(AssertionError):
-            Exponential(0.0)
+            Exponential(scale=0.0)
 
     def test_valid_params(self):
-        Exponential(1.0)
-        Exponential(1.0, name="test")
-        Exponential(jnp.ones((3, 2)))
+        Exponential(scale=1.0)
+        Exponential(scale=1.0, name="test")
+        Exponential(scale=jnp.ones((3, 2)))
 
     def test_cdf_x(self):
-        lambdas = jnp.array([1.0, 2.0, 3.0])
-        exp = Exponential(lambdas)
+        scale = jnp.array([1.0, 0.5, 1.0 / 3.0])
+        exp = Exponential(scale=scale)
         assert jnp.allclose(
             exp.cdf_x(0.0),
             jnp.zeros((3,)),
         )
         assert jnp.allclose(
             exp.cdf_x(1.0),
-            1.0 - jnp.exp(-1.0 * lambdas),
+            1.0 - jnp.exp(-1.0 / scale),
         )
         assert jnp.allclose(
             exp.cdf_x(2.0),
-            1.0 - jnp.exp(-2.0 * lambdas),
+            1.0 - jnp.exp(-2.0 / scale),
         )
 
     def test_ppf_x(self):
-        lambdas = jnp.array([1.0, 2.0, 3.0])
-        exp = Exponential(lambdas)
+        scale = jnp.array([1.0, 2.0, 3.0])
+        exp = Exponential(scale=scale)
         assert jnp.allclose(
             exp.ppf_x(0.0),
             jnp.zeros((3,)),
         )
         assert jnp.allclose(
-            exp.ppf_x(1.0 - jnp.exp(-lambdas)),
+            exp.ppf_x(1.0 - jnp.exp(-1.0 / scale)),
             1.0,
         )
         assert jnp.allclose(
-            exp.ppf_x(1.0 - jnp.exp(-2.0 * lambdas)),
+            exp.ppf_x(1.0 - jnp.exp(-2.0 / scale)),
             2.0,
         )
 
     def test_rvs(self):
-        lambdas = jnp.linspace(0.1, 10.0, 10)
-        exp = Exponential(lambdas)
+        scale = jnp.linspace(0.1, 10.0, 10)
+        exp = Exponential(scale=scale)
         key = jax.random.PRNGKey(0)
-        shape = (1000,)
+        shape = (10000,)
         rvs = exp.rvs(shape, key)
-        print(
-            jnp.var(rvs, axis=(0,)),
-            jnp.power(lambdas, -2.0),
-        )
-        assert rvs.shape == shape + lambdas.shape
+        print(jnp.mean(rvs, axis=(0,)))
+        assert rvs.shape == shape + scale.shape
         assert jnp.all(rvs >= 0.0)
         assert jnp.allclose(
             jnp.mean(rvs, axis=(0,)),
-            1.0 / lambdas,
-            atol=0.1,
+            scale,
+            atol=0.15,
         )
